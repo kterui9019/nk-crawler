a7fe98292cb34bda1454784c5c1f71ba11bbe8bd
kterui9019 <pleasure.teru@gmail.com>
2021-09-11 15:31:01 +0900

    v0.1

--- run.mjs
+++ run.mjs
@@ -1,25 +1,40 @@
 #!/usr/bin/env zx
-const years = ["2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021"]
-const months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
+const years = ["2020"];
+const months = [
+  "01",
+  "02",
+  "03",
+  "04",
+  "05",
+  "06",
+  "07",
+  "08",
+  "09",
+  "10",
+  "11",
+  "12",
+];
 
+const raceHeader =
+  "id,date,name,start_time,cource,distance,direction,weather,going,round,field,day_round,conditions,class,symbol,weight,headage,1st_prize,2nd_prize,3rd_prize,4th_prize,5th_prize";
+const raceResultHeader =
+  "id,raceId,order,flame,horse_number,horse_id,sex,age,basis_weight,jockey_id,time,finish_diff,popular,odds,furlong,corner_passing_order,trainer_id,horse_weight,horse_weight_diff,前走レースID,前前走レースID";
 
 // write header
 const ls = (await ($`ls ./out`)).stdout.split("\n");
 if (!ls.includes("race.csv")) {
-  await ($
-    `echo "レースID,レース日,レース名,発走時刻,コース,天気,馬場,回数,会場,日数,条件,クラス,記号,負担重量,頭数,1位賞金,2位賞金,3位賞金,4位賞金,5位賞金" > ./out/race.csv`);
+  await ($`echo ${raceHeader} > ./out/race.csv`);
 }
 if (!ls.includes("raceResult.csv")) {
-  await ($
-    `echo "レースID,着順,枠,馬番,馬名,性齢,斤量,騎手,タイム,着差,人気,単勝オッズ,後3F,コーナー通過順,厩舎,馬体重(増減),前走レースID,前前走レースID" > ./out/raceResult.csv`);
+  await ($`echo ${raceResultHeader} > ./out/raceResult.csv`);
 }
 
 for (const y of years) {
   for (const m of months) {
     await ($
       `deno run --allow-net --allow-write --allow-read --unstable ./src/main.ts ${y} ${m}`);
-    await ($`cat ./out/race${y}${m}.csv > ./out/race.csv`);
-    await ($`cat ./out/result${y}${m}.csv > ./out/raceResult.csv`);
+    await ($`cat ./out/race${y}${m}.csv >> ./out/race.csv`);
+    await ($`cat ./out/result${y}${m}.csv >> ./out/raceResult.csv`);
     await ($`rm ./out/race${y}${m}.csv`);
     await ($`rm ./out/result${y}${m}.csv`);
   }
